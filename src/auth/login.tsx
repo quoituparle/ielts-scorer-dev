7c8476db5fd2a93e5381125ca2cc0db12c4308f2
quoituparle <daiyue.han@outlook.com>
2025-08-20 23:24:58 +0800

    some functions

--- src/auth/login.tsx
+++ src/auth/login.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from 'react-router-dom';
 import 'auth.css'
+import '../App.css'
 import axios from 'axios'
 import apiClient from "../axiosConfig";
 
